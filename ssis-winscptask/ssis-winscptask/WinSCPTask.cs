d9d963bc81ad1780d1b316208037bbc9b9aa78f6
Bartosz Ratajczyk <b.ratajczyk@gmail.com>
2016-03-20 22:09:13 +0100

    First version with control available in SSIS Toolbox

---
+++ ssis-winscptask/ssis-winscptask/WinSCPTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace ssis_winscptask
+{
+    [DtsTask(
+        Description = "WinSCP Task - obsługa SFTP, FTP/TLS, SCP za pomocą biblioteki WinSCP",
+        DisplayName = "WinSCP Task"
+    )]
+    public class WinScpTask : Task
+    {
+        public override DTSExecResult Execute(Connections connections, VariableDispenser variableDispenser, IDTSComponentEvents componentEvents, IDTSLogging log, object transaction)
+        {
+            return base.Execute(connections, variableDispenser, componentEvents, log, transaction);
+        }
+    }
+}
