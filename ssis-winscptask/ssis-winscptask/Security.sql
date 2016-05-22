af6a0eff8c7ac8421caa0bbe9c6333ff2d7dd074
Bartosz Ratajczyk <b.ratajczyk@gmail.com>
2016-05-22 15:53:29 +0200

    Creating login and user for ssistaskdb database

---
+++ ssis-winscptask/ssis-winscptask/Security.sql
@@ -0,0 +1,19 @@
+﻿USE [master]
+GO
+
+CREATE LOGIN [ssis-winscptask] 
+WITH PASSWORD=N'123qwe!@#', 
+DEFAULT_DATABASE=[ssistaskdb], 
+CHECK_EXPIRATION=OFF, 
+CHECK_POLICY=OFF
+GO
+
+USE [ssistaskdb]
+GO
+CREATE USER [ssis-winscptask] FOR LOGIN [ssis-winscptask]
+GO
+
+USE [ssistaskdb]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [ssis-winscptask]
+GO
