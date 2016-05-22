4346af17625f82b400880f7eb9c2921c72ac117e
Bartosz Ratajczyk <b.ratajczyk@gmail.com>
2016-05-22 15:54:20 +0200

    Namespace changes; trying LocalFileInfo class for storing file information

---
+++ ssis-winscptask/ssis-winscptask/LocalFileInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace BartekR.WinSCP.CustomTask
+{
+    public class LocalFileInfo
+    {
+        public string FullName { get; set; }
+
+        public string Name { get; set; }
+    }
+}
