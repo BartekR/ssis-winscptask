8a31f7c35552d9aaeb2cf186034c97842c79a892
Bartosz Ratajczyk <b.ratajczyk@gmail.com>
2017-01-08 21:03:38 +0100

    Dodanie czasu odczytu danych z serwera

--- ssis-winscptask/ssis-winscptask/Tables.sql
+++ ssis-winscptask/ssis-winscptask/Tables.sql
@@ -77,7 +77,10 @@ GO
 CREATE TABLE dbo.ServerFiles (
 	RemoteFilePath		VARCHAR(255)						NOT NULL,
 	RemoteDirectoryName	VARCHAR(255)						NOT NULL,
-	LocalFileName		VARCHAR(255)						NOT NULL
+	LocalFileName		VARCHAR(255)						NOT NULL,
+	ReadDTTM			DATETIME2(0)
+		CONSTRAINT DF_ServerFiles_ReadDTTM
+			DEFAULT SYSUTCDATETIME()						NOT NULL
 );
 GO
 
